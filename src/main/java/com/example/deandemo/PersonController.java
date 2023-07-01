b8a8c0dce98e2c1e79295d35cedd78187ab37514
git-leon <xleonhunter@gmail.com>
2023-06-30 22:03:46 -0400

    added dockerfile

--- src/main/java/com/example/deandemo/PersonController.java
+++ src/main/java/com/example/deandemo/PersonController.java
@@ -1,9 +1,11 @@
 package com.example.deandemo;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,5 +21,10 @@ public class PersonController {
     public ResponseEntity<Person> create(Person personToPersist) {
         Person personInDatabase = personRepository.save(personToPersist);
         return new ResponseEntity<>(personInDatabase, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<String> page() {
+        return new ResponseEntity<>("hello world", HttpStatus.OK);
     }
 }
